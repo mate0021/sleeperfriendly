ebbc3400ac4ae9189d2a6cb000a538524401ea6d
mate00 <mami.ext@gmail.com>
2015-02-06 17:27:25 +0100

    Sorted out issue with class version (update-alternatives set to 1.7).

--- app/src/androidTest/java/pl/mate00/sleeperfriendlyapp/timeline/db/TimelineDbTest.java
+++ app/src/androidTest/java/pl/mate00/sleeperfriendlyapp/timeline/db/TimelineDbTest.java
@@ -5,19 +5,30 @@ import android.content.Context;
 import junit.framework.TestCase;
 
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
 import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
 
+import static junit.framework.Assert.assertTrue;
+
+@Config(emulateSdk = 18)
 @RunWith(RobolectricTestRunner.class)
-public class TimelineDbTest extends TestCase {
+public class TimelineDbTest  {
 
     private TimelineDb subject;
     private Context context = Robolectric.application;
 
-//    @Before
-//    public void setUp() {
-//        subject = new TimelineDb(context);
-//    }
+    @Before
+    public void setUp() {
+        subject = new TimelineDb(context);
+    }
+
+    @Test
+    public void timelineShouldBeEmpty() {
+        System.out.println(subject.getNumberOfAlarms());
+        assertTrue(subject.isEmpty());
+    }
 
 }
