a30429e463d158c5fd1c1d220a14e3000663c1f0
mate00 <mami.ext@gmail.com>
2015-04-07 13:48:04 +0200

    When fired alarm, check next closest alarm.

--- app/src/main/java/pl/mate00/sleeperfriendlyapp/NextAlarmReceiver.java
+++ app/src/main/java/pl/mate00/sleeperfriendlyapp/NextAlarmReceiver.java
@@ -6,6 +6,15 @@ import android.content.Intent;
 import android.util.Log;
 import android.widget.Toast;
 
+import com.google.common.base.Optional;
+
+import org.joda.time.DateTime;
+
+import pl.mate00.sleeperfriendlyapp.timeline.Alarm;
+import pl.mate00.sleeperfriendlyapp.timeline.Timeline;
+import pl.mate00.sleeperfriendlyapp.timeline.db.TimelineDb;
+import pl.mate00.sleeperfriendlyapp.ui.AlarmManagerMobile;
+
 public class NextAlarmReceiver extends BroadcastReceiver {
     private static final String TAG = NextAlarmReceiver.class.getSimpleName();
 
@@ -16,5 +25,11 @@ public class NextAlarmReceiver extends BroadcastReceiver {
     public void onReceive(Context context, Intent intent) {
         Log.d(TAG, "alarm fired");
         Toast.makeText(context, "Alarm", Toast.LENGTH_LONG).show();
+        Timeline timeline = new TimelineDb(context);
+        Optional<Alarm> nextAlarm = timeline.getClosestTo(new DateTime());
+        if (nextAlarm.isPresent()) {
+            AlarmManagerMobile alarmManager = new AlarmManagerMobile(context);
+            alarmManager.updateWithClosestAlarm(nextAlarm.get());
+        }
     }
 }
